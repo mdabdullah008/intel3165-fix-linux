db65c282756de4a936ffb64222390e899acf5db3
Mohammed Abdullah (Ashfaq) <ipsc42mohammedabdullah@gmail.com>
2025-02-23 23:59:58 +0600

    fixed mentions

--- iwlwifi-fix.sh
+++ iwlwifi-fix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-echo "Wi-Fi workaround fix for Intel Wireless 3165 script by @mdabdullah008"
+echo "Wi-Fi workaround fix for Intel Wireless 3165 script by @suniic"
 echo "Please enter your password" 
 sudo modprobe -r iwlwifi
 echo "Restarting Wi-Fi module"  
